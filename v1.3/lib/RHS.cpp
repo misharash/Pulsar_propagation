6023eaf39ab7b60fe019df73447cb17e3643063b
Михаил Рашковецкий <misharash@gmail.com>
2017-03-21 02:13:36 +0300

    Examining process_functions in RHS

--- v1.3/lib/RHS.cpp
+++ v1.3/lib/RHS.cpp
@@ -14,6 +14,9 @@ void RHS(double R, double *f, double *dydx) {
 	double QQ = Q (R);
 	double BB = BetaB (R);
 	double DD = delta (R);
+	
+	//temp for understanding
+	cout << R << " " << LL << " " << QQ << " " << BB << " " << DD << endl;
 
 	dydx[0] = coeff * (-LL / QQ - LL * cos(2 * f[0] - 2 * BB - 2 * DD) * sinh(2 * f[1]));
 	dydx[1] = coeff * LL * sin(2 * f[0] - 2 * BB - 2 * DD) * cosh(2 * f[1]);
