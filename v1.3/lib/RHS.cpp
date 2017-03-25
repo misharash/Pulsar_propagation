e60045fa90127705414424ab956d058d43affd9f
Михаил Рашковецкий <misharash@gmail.com>
2017-03-25 13:09:45 +0300

    Cleaned integrator table

--- v1.3/lib/RHS.cpp
+++ v1.3/lib/RHS.cpp
@@ -20,8 +20,8 @@ void RHS(double R, double *f, double *dydx) {
 
 	dydx[0] = coeff * (-LL / QQ - LL * cos(2 * f[0] - 2 * BB - 2 * DD) * sinh(2 * f[1]));
 	dydx[1] = coeff * LL * sin(2 * f[0] - 2 * BB - 2 * DD) * cosh(2 * f[1]);
-	//temp for debug
+	/*/temp for debug
 	if (fabs(dydx[1])>1e5) {
 		cout << R << " " << f[0] << " " << f[1] << " " << coeff << " " << LL << endl;
-	}
+	}*/
 }
--- v1.3/lib/diffeqsolver.cpp
+++ v1.3/lib/diffeqsolver.cpp
@@ -175,7 +175,7 @@ void odeint(
         }
         if ((x+h-x2)*(x+h-x1) > 0.0) h=x2-x;
         
-        plot << y[0] << " " << y[1] << " " << dydx[0] << " " << dydx[1] << " " << x << endl; //temp
+        //plot << y[0] << " " << y[1] << " " << dydx[0] << " " << dydx[1] << " " << x << endl;
 
         (rkqc)(y, dydx, nvar, &x, h, eps, yscal, &hdid, &hnext, derivs);
 
